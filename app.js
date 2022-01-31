df4e4d6379a2fab70a2615d912d353a22c1ffbb8
acosta-gonzalo-agustin <acosta.gonzalo.agustin@gmail.com>
2022-01-31 20:55:01 -0300

    subiendo proyecto

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log('servidor montado');
+});
+
+
+app.get('/home', function(req,res) {
+    res.sendFile(path.join(__dirname,'views/home.html'));
+});
+
+app.get('/register', function(req,res) {
+    res.sendFile(path.join(__dirname,'views/register.html'));
+});
+
+app.get('/login', function(req,res) {
+    res.sendFile(path.join(__dirname,'views/login.html'));
+});
+
+app.post('/home', function(req,res) {
+    res.sendFile(path.join(__dirname,'views/home.html'));
+});
+
+
